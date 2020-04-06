5d073d52981d483c730bc02876a7709b77e94575
Piyush Srivastava <piyush.srivastava@tifr.res.in>
2020-04-06 13:33:23 +0530

    CPP: node initialization from input files

--- cpp-simulator/initializers.cc
+++ cpp-simulator/initializers.cc
@@ -54,7 +54,7 @@ vector<workplace> init_workplaces() {
 
   auto wp_size = wpJSON.GetArray().Size();
   GLOBAL.num_workplaces = wp_size;
-  
+
   auto size = wp_size +  school_size;
   vector<workplace> wps(size);
 
@@ -96,9 +96,105 @@ vector<community> init_community() {
 }
 
 
-int main(){
+vector<double> compute_prob_infection_given_community(double infection_probability,bool set_uniform){
+  auto fracPopJSON = readJSONFile(input_base + "fractionPopulation.json");
+  auto num_communities = fracPopJSON.GetArray().Size();
+  if(set_uniform){
+	return vector<double>(num_communities, infection_probability);
+  }
+  else {
+	auto fracQuarantinesJSON = readJSONFile(input_base + "quarantinedPopulation.json");
+	const rapidjson::Value& quar_array = fracQuarantinesJSON.GetArray();
+	vector<double> prob_infec_given_community(num_communities);
+	int index = 0;
+	for(auto &elem: fracPopJSON.GetArray()){
+	  prob_infec_given_community[index] =
+		infection_probability *
+		quar_array[index]["fracQuarantined"].GetDouble() /
+		elem["fracPopulation"].GetDouble();
+	  ++index;
+	}
+	return prob_infec_given_community;
+  }
+}
+
+
+vector<agent> init_nodes(){
+  auto indivJSON = readJSONFile(input_base + "individuals.json");
+  auto size = indivJSON.GetArray().Size();
+  GLOBAL.num_people = size;
+  vector<agent> nodes(size);
+  auto community_infection_prob = compute_prob_infection_given_community(GLOBAL.INIT_FRAC_INFECTED, false);
+
+  int i = 0;
+  for (auto &elem: indivJSON.GetArray()){
+	nodes[i].loc = location{elem["lat"].GetDouble(),
+							elem["lon"].GetDouble()};
+	int age = elem["age"].GetInt();
+	nodes[i].age = age;
+	nodes[i].age_group = get_age_group(age);
+	nodes[i].zeta_a = zeta(age);
+
+
+	nodes[i].home = elem["household"].GetInt();
+
+	nodes[i].workplace = WORKPLACE_HOME; //null workplace, by default
+	nodes[i].workplace_type = WorkplaceType::home; //home, by default
+	switch(elem["workplaceType"].GetInt()){
+	case 1:
+	  if(elem["workplace"].IsNumber()){
+		nodes[i].workplace_type = WorkplaceType::office;
+		nodes[i].workplace = int(elem["workplace"].GetDouble());
+	  }
+	  break;
+	case 2:
+	  if(elem["school"].IsNumber()){
+		nodes[i].workplace_type = WorkplaceType::school;
+		nodes[i].workplace = int(elem["school"].GetDouble());
+	  }
+	default:
+	  break;
+	}
+
+	int community = elem["wardNo"].GetInt() - 1;
+	//minus 1 for 0-based indexing.  POSSIBLE BUG: Might need to use
+	//"wardIndex" instead, because that is the one actually sent by
+	//the generator scripts.
+	nodes[i].community = community;
+	if(bernoulli(community_infection_prob[community])){
+	  nodes[i].infection_status = Progression::exposed;
+	} else {
+	  nodes[i].infection_status = Progression::susceptible;
+	}
+
+	nodes[i].funct_d_ck = f_kernel(elem["CommunityCentreDistance"].GetDouble());
+
+
+	if(SEED_INFECTION_FROM_FILE){
+	  nodes[i].infection_status = static_cast<Progression>(elem["infection_status"].GetInt());
+	  nodes[i].time_of_infection = (nodes[i].infection_status == Progression::exposed)?(-elem["time_since_infected"].GetDouble()):0;
+	} else {
+	  nodes[i].time_of_infection = (nodes[i].infection_status == Progression::exposed)?(-uniform_real(0, nodes[i].incubation_period)):0;
+	  //Why do we take time of infection to be 0 by default?
+	}
+	//Set infective status, set the time of infection, and other age-related factors
+	nodes[i].infective = (nodes[i].infection_status == Progression::infective);
+	//POSSIBLE BUG: In the JS code, and here, infection_status is
+	//never set to infective, so the above line would be superfluous!
+
+	++i;
+  }
+  return nodes;
+}
+
+void run_simulation(){
   auto homes = init_homes();
   auto workplaces = init_workplaces();
   auto communities = init_community();
+  auto nodes = init_nodes();
+}
+
+int main(){
+  run_simulation();
   return 0;
 }
--- cpp-simulator/models.h
+++ cpp-simulator/models.h
@@ -2,11 +2,27 @@
 #define MODELS_H_
 #include <vector>
 #include <random>
-
+#include <tuple>
+#include <cmath>
 
 // Global parameters functions
 std::default_random_engine GENERATOR;
 
+inline double gamma(double shape, double scale){
+  return std::gamma_distribution<double>(shape, scale)(GENERATOR);
+}
+
+inline bool bernoulli(double p){
+  return std::bernoulli_distribution(p)(GENERATOR);
+}
+
+inline double uniform_real(double left, double right){
+  return std::uniform_real_distribution<double>(left, right)(GENERATOR);
+}
+
+//These are parameters associated with the disease progression
+const double NUM_DAYS_TO_RECOG_SYMPTOMS = 1;
+const bool SEED_INFECTION_FROM_FILE = false;
 // Global parameters
 //
 // The default values are as in the js simulator.  These are changed
@@ -18,13 +34,84 @@ struct global_params{
   int num_workplaces = 5000;
   int num_schools = 0;
   int num_communities = 198;
-  
+
+  int num_people = 100000;
+
+  int NUM_DAYS = 120; //Number of days. Simulation duration
+  int SIM_STEPS_PER_DAY = 4; //Number of simulation steps per day.
+  int NUM_TIMESTEPS = NUM_DAYS*SIM_STEPS_PER_DAY; //
+  double INIT_FRAC_INFECTED = 0.0001; // Initial number of people infected
+
+  double INCUBATION_PERIOD = 2.25;
+  double MEAN_ASYMPTOMATIC_PERIOD = 0.5;
+  double MEAN_SYMPTOMATIC_PERIOD = 5;
+  double MEAN_HOSPITAL_REGULAR_PERIOD = 8;
+  double MEAN_HOSPITAL_CRITICAL_PERIOD = 8;
+
+  double INCUBATION_PERIOD_SHAPE = 2;
+  double INCUBATION_PERIOD_SCALE = INCUBATION_PERIOD*SIM_STEPS_PER_DAY;// 2.29 days
+
+  double INFECTIOUSNES_SHAPE = 0.25;
+  double INFECTIOUSNES_SCALE = 4;
+
+  double SEVERITY_RATE = 0.5; //value used in sim.js
+
+  double ASYMPTOMATIC_PERIOD = MEAN_ASYMPTOMATIC_PERIOD*SIM_STEPS_PER_DAY;
+  // half a day
+  double SYMPTOMATIC_PERIOD = MEAN_SYMPTOMATIC_PERIOD*SIM_STEPS_PER_DAY;
+  // 5 days
+  double HOSPITAL_REGULAR_PERIOD = MEAN_HOSPITAL_REGULAR_PERIOD*SIM_STEPS_PER_DAY;
+  double HOSPITAL_CRITICAL_PERIOD = MEAN_HOSPITAL_CRITICAL_PERIOD*SIM_STEPS_PER_DAY;
+  double SYMPTOMATIC_FRACTION = 0.67;
+
 } GLOBAL;
 
 // return a random compliance based on GLOBAL.compliance_probability
 inline bool compliance(){
-  return std::bernoulli_distribution(GLOBAL.compliance_probability)(GENERATOR);
+  return bernoulli(GLOBAL.compliance_probability);
 }
+
+//Age groups (5-years)
+
+const int NUM_AGE_GROUPS = 16;
+int get_age_group(int age){
+  int age_group = age/5;
+  return std::min(age_group, NUM_AGE_GROUPS - 1);
+}
+
+
+double zeta(int age){
+	// This might change based on better age-related interaction data.
+  if(age < 5) {
+    return 0.1;
+  } else if(age < 10) {
+    return 0.25;
+  } else if(age < 15) {
+    return 0.5;
+  } else if(age < 20) {
+    return 0.75;
+  } else if(age < 65) {
+    return 1;
+  } else if(age < 70) {
+    return 0.75;
+  } else if(age < 75) {
+    return 0.5;
+  } else if(age < 85) {
+    return 0.25;
+  } else {
+    return 0.1;
+  }
+}
+
+
+double f_kernel(double dist){
+  double a = 4; // for distance in kms
+  double b = 3.8;
+  //both values are for Thailand, until we get a fit for India
+  //Same as in the JS file
+  return 1/(1 + pow(dist/a,b));
+}
+
 
 // End of global parameters
 
@@ -56,6 +143,15 @@ enum class Intervention {
 };
 
 
+enum class WorkplaceType{
+   office = 1,
+   school = 2,
+   home = 0
+};
+
+//Default workplace value for homebound individuals.
+const int WORKPLACE_HOME = -1;
+
 // Time varying properties of agents.
 //
 // Eg: whether the agent is infected, whether it is
@@ -68,9 +164,62 @@ struct agentState {
 struct  agentProps {
 };
 
+
 struct agent{
-  agentState state;
-  const agentProps props;
+  location loc;
+  int age;
+  int age_group;
+  double zeta_a = 1;
+  double infectiousnes = gamma(GLOBAL.INFECTIOUSNES_SHAPE, GLOBAL.INFECTIOUSNES_SCALE);
+  //a.k.a rho
+  double severity = bernoulli(GLOBAL.SEVERITY_RATE)?1:0;
+  //a.k.a S_k, is 0 or
+  int home; //index of household
+  int workplace;
+
+  int community;
+  double time_of_infection;
+  Progression infection_status;
+  bool infective = false;
+
+  double lambda_h = 0;
+  //individuals contribution to his home cluster
+  double lambda_w = 0;
+  //individuals contribution to his workplace cluster
+  double lambda_c = 0;
+  //individuals contribution to his community
+  double lambda = 0;
+
+  double kappa_T = 1;
+  double psi_T = -0;
+  double funct_d_ck;
+
+  WorkplaceType workplace_type;
+  //one of school, office, or home
+  std::tuple<double, double, double> lambda_incoming;
+  //infectiousness from home, workplace, community as seen by
+  //individual
+
+
+  bool compliant = true;
+  double kappa_H = 1;
+  double kappa_W = 1;
+  double kappa_C = 1;
+
+  double incubation_period = gamma(GLOBAL.INCUBATION_PERIOD_SHAPE, GLOBAL.INCUBATION_PERIOD_SCALE);
+  double asymptomatic_period = gamma(1, GLOBAL.ASYMPTOMATIC_PERIOD);
+  double symptomatic_period = gamma(1, GLOBAL.SYMPTOMATIC_PERIOD);
+
+  double hospital_regular_period = GLOBAL.HOSPITAL_REGULAR_PERIOD;
+  double hospital_critical_period = GLOBAL.HOSPITAL_CRITICAL_PERIOD;
+
+  double kappa_H_incoming = 1;
+  double kappa_W_incoming = 1;
+  double kappa_C_incoming = 1;
+  bool quarantined = false;
+
+  agent(){}
+
 };
 
 
@@ -93,10 +242,6 @@ struct house{
   }
 };
 
-enum class WorkplaceType{
-   office = 1,
-   school = 2
-};
 
 struct workplace {
   location loc;
@@ -137,4 +282,3 @@ struct community {
 };
 
 #endif
-

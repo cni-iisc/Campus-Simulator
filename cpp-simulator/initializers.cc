91988e81242df8dbb75748d8e1318f9071997cd2
Piyush Srivastava <piyush.srivastava@tifr.res.in>
2020-04-06 09:49:17 +0530

    Some of the first JSON initializers

---
+++ cpp-simulator/initializers.cc
@@ -0,0 +1,84 @@
+#include <rapidjson/document.h>
+#include <rapidjson/istreamwrapper.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+
+#include "models.h"
+
+using namespace std;
+
+const string input_base = "../simulator/input_files/";
+
+auto readJSONFile(string filename){
+  ifstream ifs(filename, ifstream::in);
+  rapidjson::IStreamWrapper isw(ifs);
+  rapidjson::Document d;
+  d.ParseStream(isw);
+  return d;
+}
+
+auto prettyJSON(const rapidjson::Document& d){
+  rapidjson::StringBuffer buffer;
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+  d.Accept(writer);
+  return buffer.GetString();
+}
+
+
+vector<house> init_homes(){
+  auto houseJSON = readJSONFile(input_base + "houses.json");
+  auto size = houseJSON.GetArray().Size();
+  vector<house> homes(size);
+  GLOBAL.num_homes = size;
+
+  int index = 0;
+  for (auto &elem: houseJSON.GetArray()){
+	homes[index].set(elem["lat"].GetDouble(),
+					 elem["lon"].GetDouble(),
+					 compliance());
+	++index;
+  }
+  return homes;
+}
+
+vector<workplace> init_workplaces() {
+  auto schoolJSON = readJSONFile(input_base + "schools.json");
+  auto wpJSON = readJSONFile(input_base + "workplaces.json");
+
+  auto school_size = schoolJSON.GetArray().Size();
+  GLOBAL.num_schools = school_size;
+
+  auto wp_size = wpJSON.GetArray().Size();
+  GLOBAL.num_workplaces = wp_size;
+  
+  auto size = wp_size +  school_size;
+  vector<workplace> wps(size);
+
+  int index = 0;
+  // schools come first followed by workspaces, as in the JSON version
+  for (auto &elem: schoolJSON.GetArray()){
+	wps[index].set(elem["lat"].GetDouble(),
+				   elem["lon"].GetDouble(),
+				   WorkplaceType::school);
+	++index;
+  }
+
+  for (auto &elem: wpJSON.GetArray()){
+	wps[index].set(elem["lat"].GetDouble(),
+				   elem["lon"].GetDouble(),
+				   WorkplaceType::office);
+	++index;
+  }
+
+  return wps;
+}
+
+int main(){
+  auto homes = init_homes();
+  auto workplaces = init_workplaces();
+  return 0;
+}

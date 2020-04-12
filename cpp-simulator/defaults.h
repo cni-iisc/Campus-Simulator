020d9a495dac42012a7a4d9b103f6f0dad85f294
Ramprasad Saptharishi <ramprasad@tifr.res.in>
2020-04-12 08:16:18 +0530

    Changing to IISc betas, and beta_travel=0

--- cpp-simulator/defaults.h
+++ cpp-simulator/defaults.h
@@ -13,11 +13,11 @@ struct defaults{
   std::string MEAN_HOSPITAL_REGULAR_PERIOD = "8";
   std::string MEAN_HOSPITAL_CRITICAL_PERIOD = "8";
   std::string COMPLIANCE_PROBABILITY = "0.9";
-  std::string BETA_H = "0.47";
-  std::string BETA_W = "0.94";
-  std::string BETA_C = "0.47045";
-  std::string BETA_S = "1.88";
-  std::string BETA_TRAVEL = "10.0";
+  std::string BETA_H = "1.227";
+  std::string BETA_W = "0.919";
+  std::string BETA_C = "0.233";
+  std::string BETA_S = "1.820";
+  std::string BETA_TRAVEL = "0.0";
   std::string HD_AREA_FACTOR = "2.0";
   std::string HD_AREA_EXPONENT = "0";
   std::string INTERVENTION = "0";

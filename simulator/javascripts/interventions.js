db18e801ecf14dc4065218cbb5d0dcbf0e7de59d
Nidhin <knidhin@iisc.ac.in>
2020-04-14 10:55:34 +0530

    Add case isolation as a part of school closure.

--- simulator/javascripts/interventions.js
+++ simulator/javascripts/interventions.js
@@ -18,6 +18,7 @@ function get_kappa_no_intervention(nodes, homes, workplaces, communities,cur_tim
 
 function get_kappa_SC(nodes, homes, workplaces, communities,cur_time){
 	for (var count = 0; count < nodes.length;count ++){
+		var time_since_symptoms = cur_time - (nodes[count]['time_of_infection']+nodes[count]['incubation_period']+nodes[count]['asymptomatic_period']);
 		nodes[count]['kappa_T'] = kappa_T(nodes[count], cur_time);
 		nodes[count]['kappa_H'] = 1;
 		nodes[count]['kappa_W'] = 1;
@@ -25,13 +26,26 @@ function get_kappa_SC(nodes, homes, workplaces, communities,cur_time){
 		nodes[count]['kappa_PT'] = 1;
 		nodes[count]['quarantined'] = false;
 		nodes[count]['kappa_H_incoming'] = 1;
-		nodes[count]['kappa_W_incoming'] = 1;
+		nodes[count]['kappa_W_incoming'] = 1; 
 		nodes[count]['kappa_C_incoming'] = 1;
 		nodes[count]['kappa_PT_incoming'] = 1;
+
+
+		if((nodes[count]['compliant']) && 
+		 (time_since_symptoms > NUM_DAYS_TO_RECOG_SYMPTOMS*SIM_STEPS_PER_DAY) && 
+		 (time_since_symptoms <= (NUM_DAYS_TO_RECOG_SYMPTOMS+SELF_ISOLATION_DAYS)*SIM_STEPS_PER_DAY)){  
+			nodes[count]['quarantined'] = true;
+			nodes[count]['kappa_W'] = 0.25;
+			nodes[count]['kappa_C'] = 0.25;
+			nodes[count]['kappa_PT'] = 0.25;
+			nodes[count]['kappa_W_incoming'] = 0.25; 
+			nodes[count]['kappa_C_incoming'] = 0.25;
+			nodes[count]['kappa_PT_incoming'] = 0.25;
+		}
 		if(nodes[count]['workplace_type']==WTYPE_SCHOOL){
 			nodes[count]['kappa_W'] = 0;
-			nodes[count]['kappa_W_incoming'] = 0;
 			nodes[count]['kappa_PT'] = 0;
+			nodes[count]['kappa_W_incoming'] = 0;
 			nodes[count]['kappa_PT_incoming'] = 0;
 		}
 	}
@@ -377,7 +391,7 @@ function get_kappa_LOCKDOWN_21(nodes, homes, workplaces, communities,cur_time){
 function get_kappa_LD_21_CI_HQ_SD70_SC_21_SC_42(nodes, homes, workplaces, communities,cur_time){
 	const FIRST_PERIOD = 21;
 	const SECOND_PERIOD = 21;
-	const THIRD_PERIOD = 42
+	const THIRD_PERIOD = 42;
 	if(cur_time < (NUM_DAYS_BEFORE_INTERVENTIONS + FIRST_PERIOD)*SIM_STEPS_PER_DAY){
 		get_kappa_lockdown(nodes, homes, workplaces, communities,cur_time);
 	} else if(cur_time < (NUM_DAYS_BEFORE_INTERVENTIONS + FIRST_PERIOD + SECOND_PERIOD)*SIM_STEPS_PER_DAY){

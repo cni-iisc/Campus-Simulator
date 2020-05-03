8cfb178009c2ff91f02e8930d2d5210154d3e911
Preetam Patil <preetampatil@iisc.ac.in>
2020-05-03 21:49:10 +0530

    Updated questionnaire upto toilet score

--- workplace_readiness_calc/scripts.js
+++ workplace_readiness_calc/scripts.js
@@ -26,14 +26,34 @@ function calcScore () {
 	var tGapShift = parseFloat(document.getElementById("tGapShift").value);
 	var nGentsT = parseInt(document.getElementById("nGentsT").value);
 	var nLadiesT = parseInt(document.getElementById("nLadiesT").value);
-	    
+	var tCleanFreq = parseInt(document.getElementById("tCleanFreq").value);
+	
+    //Read all booleans here:
+	var soapDisp_flag = parseInt(document.querySelector('input[name="soapDisp_flag"]:checked').value);
+	console.log("SoapDisp: " + soapDisp_flag)
+
+    var nUsersG = nM + nOth/2.0;
+    var nUsersL = nF + nOth/2.0;
+    var nAVT = 5;
+    var durAVT = 4;
+    var tConcHr = 4;
+    //var scoreGentsToilet = NgentsToilet * AveToiletVisitsPerDay * AveToiletDuration / NgentsToilet / (ToiletConcHrs * 60) * max(0.5 - NgentsCleaning*0.1) * (1 - 0.1*soapDispensed)
+    var cRateGentsToilet = nUsersG * nAVT * durAVT * (Math.max(0.5, (1.0 - 0.1*tCleanFreq) )) * (1.0 - 0.1*soapDisp_flag) / (tConcHr*60*nGentsT);
+    var score_GentsToilet = cRateGentsToilet;
+    var cRateLadiesToilet = nUsersL * nAVT * durAVT * (Math.max(0.5, (1.0 - 0.1*tCleanFreq) )) * (1.0 - 0.1*soapDisp_flag) / (tConcHr*60*nLadiesT);
+    var score_LadiesToilet = cRateLadiesToilet;
+    //var score_GentsToilet = 1000 - cRateGentsToilet*1000;
+
 	var nEmp = nM + nF + nOth;
+	
 	onSuccess("Total Employees: " + nEmp);
 
 	var resTable = "<br><br><H3> Covid Readiness Score</H3>";
-	resTable += "<table><tr><td style='width: 200px; color: white;'>Category</td>";
-	resTable += "<td style='width: 100px; color: white;'>Score</td>";
-	resTable += "<td style='width: 200px; color: white;'>Improvement</td></tr>";
+	resTable += "<table><tr><td>Category</td>";
+	resTable += "<td>Score</td>";
+	resTable += "<td>Possible improvement</td></tr>";
+	resTable += "<tr><td>Gents toilets</td><td>" + score_GentsToilet + "</td><td></td></tr>"
+	resTable += "<tr><td>Ladies toilets</td><td>" + score_LadiesToilet + "</td><td></td></tr>"
 	resTable += "</table>";
 	document.getElementById("scoreTable").innerHTML = resTable;
 
@@ -68,11 +88,11 @@ function handleFormSubmit(formObject) {
   /* google.script.run.withSuccessHandler(onSuccess).processForm(formObject); */
   /* document.getElementById("myForm").reset(); */
         calcScore();
-	openPage('Scores', document.getElementById("ScoresTab"), 'darkgreen')
+	openPage('Scores', document.getElementById("ScoresTab"), '#669977')
 }
 
 function reEnter() {
-	openPage('Qn', document.getElementById("QnTab"), '#777777')
+	openPage('Qn', document.getElementById("QnTab"), '#886655')
 }
 
 

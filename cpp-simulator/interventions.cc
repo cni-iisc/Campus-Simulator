3764099ef80927c1b44b8bfcc27f39a323b5af75
Piyush Srivastava <piyushsriva@gmail.com>
2020-05-04 10:05:44 +0530

    CPP: A possible parallelization issue in get_kappa_custom

--- cpp-simulator/interventions.cc
+++ cpp-simulator/interventions.cc
@@ -477,7 +477,9 @@ void get_kappa_custom(vector<agent>& nodes, vector<house>& homes, const vector<w
 
 #pragma omp parallel for
   for (count_type count = 0; count < nodes.size(); ++count){
-	time_since_symptoms = cur_time
+	//Local variables in parallel loops should be declared inside the loop so
+	//that they are not shared across different parallel runs
+	double time_since_symptoms = cur_time
 	  - (nodes[count].time_of_infection
 		 + nodes[count].incubation_period
 		 + nodes[count].asymptomatic_period); // needed to determine case isolation status

6e1a528ac509a1ecbcb59c923674eb3d50c32700
Minhaas <minhaasmohammed@gmail.com>
2020-12-30 18:15:19 +0530

    [CPP Simulator] added periodicity

--- cpp-simulator/campus_updates.h
+++ cpp-simulator/campus_updates.h
@@ -18,13 +18,15 @@ double update_interaction_spaces(agent& node, int cur_day);
 
 double update_n_k(agent& node, int cur_day, Interaction_Space& i_space);
 
-void update_interaction_space_lambda(std::vector<agent> nodes, std::vector<Interaction_Space>& i_spaces, int cur_day);
+void update_interaction_space_lambda(std::vector<agent>& nodes, std::vector<Interaction_Space>& i_spaces, int cur_day);
 
 void update_individual_lambda(std::vector<agent>& nodes, std::vector<Interaction_Space>& i_spaces, int cur_day);
 
 void assign_individual_campus(std::vector<agent>& nodes, std::vector<Interaction_Space>& interaction_space, int cur_day);
 
-node_update_status update_infection(agent& node, int cur_time);
+//void print_interaction_strength(agent& node, std::vector<Interaction_Space>& ispace, int day);
+
+node_update_status update_infection(agent& node, int cur_time, int day);
 
 //update_all_kappa(nodes, homes, workplaces, communities, nbr_cells, intv_params, time_step);
 

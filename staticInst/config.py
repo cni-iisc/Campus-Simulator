4853b833359e757ae8ea804b6c15b999af31056c
Minhaas <minhaasmohammed@gmail.com>
2021-03-18 10:19:36 +0530

    Changed config.py

--- staticInst/config.py
+++ staticInst/config.py
@@ -3,7 +3,7 @@ import json
 markov_simuls = True
 
 config_json = []
-def configCreate(min_group_size, max_group_size, beta_scaling_factor, avg_num_assns, periodicity, minimum_hostel_time, kappa_class_case_isolation, kappa_hostel_case_isolation, kappa_mess_case_isolation, kappa_cafe_case_isolation, kappa_smaller_networks_case_isolation, kappa_class_lockdown, kappa_hostel_lockdown, kappa_mess_lockdown, kappa_cafe_lockdown, kappa_smaller_networks_lockdown):
+def configCreate(min_group_size, max_group_size, beta_scaling_factor, avg_num_assns, periodicity, minimum_hostel_time, kappa_class_case_isolation = 0.0, kappa_hostel_case_isolation = 0.2, kappa_mess_case_isolation = 0.1, kappa_cafe_case_isolation = 0.1, kappa_smaller_networks_case_isolation = 0.4, kappa_class_lockdown = 0.0, kappa_hostel_lockdown = 0.2, kappa_mess_lockdown = 0.0, kappa_cafe_lockdown = 0.0, kappa_smaller_networks_lockdown = 0.0):
     config = {}
     config["MIN_GROUP_SIZE"] = min_group_size
     config["MAX_GROUP_SIZE"] = max_group_size
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     kappa_cafe_lockdown = 0.0
     kappa_smaller_networks_lockdown = 0.0
 
-    config = configCreate(min_group_size, max_group_size, beta_scaling_factor, avg_num_assns, periodicity, min_hostel_time, kappa_class_case_isolation, kappa_hostel_case_isolation, kappa_mess_case_isolation, kappa_cafe_case_isolation, kappa_smaller_networks_case_isolation, kappa_class_lockdown, kappa_hostel_lockdown, kappa_mess_lockdown, kappa_cafe_lockdown, kappa_smaller_networks_lockdown)
+    config = configCreate(min_group_size, max_group_size, beta_scaling_factor, avg_num_assns, periodicity, min_hostel_time)
 
     f = open(output_file_dir + output_file, "w+")
     f.write(json.dumps(config))
